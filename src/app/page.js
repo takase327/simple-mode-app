ab17ad88ba17415757f1133df0987b4fb32d5b45
Tomoya Takase <takase@example.com>
2025-04-11 15:59:45 +0900

    パス修正

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
 const questions = [
